23c34b35a5d99e4252ca8a333b550c875d73b3da
dmil148 <125594009+dmil148@users.noreply.github.com>
2024-02-07 03:02:20 +0000

    dsd

--- sketch.js
+++ sketch.js
@@ -1,75 +1,69 @@
-let selectedColor;
-let choices;
-
-function setup() 
-{
-  createCanvas(1000,1000);
-  selectedColor = color('white');
-  
-  background(255);
-
-  choices = [
-    new Choice(0,0,color('red')),
-    new Choice(0,40,color('orange')),
-    new Choice(0,80,color('yellow')),
-    new Choice(0,120,color('green')),
-    new Choice(0,160,color('cyan')),
-    new Choice(0,200,color('blue')),
-    new Choice(0,240,color('magenta')),
-    new Choice(0,280,color('brown')),
-    new Choice(0,320,color('white')),
-    new Choice(0,360,color('black')),
-  ]
+function setup() {
+  createCanvas(1000, 1000);
+  angleMode(DEGREES);
+  pg1 = createGraphics(200, 100);
+  pg2 = createGraphics(300,300);
+  pg3 = createGraphics(200,100); 
+  pg4 = createGraphics(200,200);
 }
 
 function draw() {
+  background(200);
+  pg1.background('rgb(0,255,0)');
+  pg1.circle(50, 50, 75);
+  pg1.square(100,10,80);
+  image(pg1, 0, 0);
 
-  for(i = 0; i < choices.length; i++)
-  {
-    choices[i].draw();
-  }
+  pg2.background(255);
+  pg2.noStroke();
+  pg2.fill(255,0,0,100);
+  pg2.circle(pg2.width/2,pg2.height/3,150);
+  pg2.fill(0,0,255,100);
+  pg2.circle(100,200,150);
+  pg2.fill(0,255,0,100);
+  pg2.circle(200,200,150);
+  image(pg2, 0, 100);
 
-  fill(selectedColor);
+  pg3.background(0);
+  pg3.noStroke();
+  pg3.fill('yellow');
+  pg3.arc(50,50,75,75,-8.5,2.5);
+  pg3.fill('red');
+  pg3.circle(150,50,75);
+  pg3.rect(112.5,45,75,45);
+  pg3.fill('white');
+  pg3.circle(132.5,50,25);
+  pg3.circle(167.5,50,25);
+  pg3.fill('blue');
+  pg3.circle(132.5,50,15);
+  pg3.circle(167.5,50,15);
+  image(pg3, 0, 400);
+
+  pg4.background(0,0,150);
+  pg4.fill('white');
+  pg4.noStroke();
+  pg4.circle(pg4.height/2,pg4.height/2,100);
+  pg4.fill(0,150,0);
+  pg4.circle(pg4.height/2,pg4.height/2,95);
+  pg4.fill(255);
+  star(pg4,100,100,55,20,5);
+  pg4.fill(255,0,0);
+  star(pg4,100,100,45,15,5);
+  image(pg4,0,500);
 }
 
-function mousePressed()
-{
-  let isInChoice = false;
-  for(let i=0;i < choices.length;i++) {
-    if(choices[i].contains(mouseX,mouseY)) {
-      selectedColor = choices[i].fill;
-      isInChoice = true;
-    }
+
+function star(context, x, y, radius1, radius2, npoints) {
+  let angle = 360 / npoints;
+  let halfAngle = angle / 2.0;
+  context.beginShape();
+  for (let a = 0; a < 360; a += angle) {
+    let sx = x + cos(a) * radius2;
+    let sy = y + sin(a) * radius2;
+    vertex(sx, sy);
+    sx = x + cos(a + halfAngle) * radius1;
+    sy = y + sin(a + halfAngle) * radius1;
+    vertex(sx, sy);
   }
-
-  fill(selectedColor);
-  console.log("selected color is " + selectedColor);
-}
-
-function mouseDragged()
-{
-  ellipse(mouseX, mouseY, 15);
-  return false;
-}
-
-class Choice{
-  constructor(x, y, fill){
-    this.x = x;
-    this.y = y; 
-    this.fill = fill;
-  }
-
-  draw()
-  {
-    noStroke();
-    fill(this.fill);
-    square(this.x,this.y,40);
-  }
-
-  contains(x,y)
-  {
-    let insideX = x >= this.x && x <= this.x + 40;
-    let insideY = y >= this.y && y <= this.y + 40;
-    return insideX && insideY;
-  }
+  context.endShape(CLOSE);
 }
